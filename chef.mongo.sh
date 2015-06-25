65752c95ccc229a99f8e2217342cd8bb65ddd1ac
OceanYin <Ocean.Yin@Sofware.dell.com>
2015-06-25 17:26:58 +0800

    [Test] update test bash file

--- chef.mongo.sh
+++ chef.mongo.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# Author: Ocean
-
 mkdir -p /home/skytap/Downloads/tmp
 touch /home/skytap/Downloads/test.tmp
 ls -l > /home/skytap/Downloads/test.tmp
+
+# copy file from file server to Downloads
+# To check whether ssh works
+rsync ssh suite@10.6.208.3:"/mnt/share/Share/DataHubFiles/Ocean/jenkins.xml" /home/skytap/Downloads
+
+# Usually sudo command would need to input password
+# To check whether password is filled in automatically
+echo suite | sudo -S cat /etc/sudoers > /home/skytap/Downloads/sudoers.txt
+
+# To check file list under Downloads
+ls /home/skytap/Downloads -l >> /home/skytap/Downloads/sudoers.txt
